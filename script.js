85c9240f25719bbe80641dcf023ec7ec5b7b6887
Piers Ebdon <37227905+piers12@users.noreply.github.com>
2023-02-05 18:47:02 +0000

    feat: setup outline of UI

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
+const DAY_WORKING_HOURS = 9;
 
+// set present date
 function setCurrentDay() {
     const currentDay = moment().format('dddd, Do MMMM');
     $("#currentDay").text(currentDay);
 }
 
+// method that sets up work day with correct rows
+function createWorkDaySchedule() {
+    for (let i = 0; i < DAY_WORKING_HOURS; i++) {
+        // div for each row 
+        const rowEl = $("<div>");
+        rowEl.addClass("row");
+        
+        // p for hour time
+        const hourEl = $("<p>");
+        hourEl.addClass("hour col-1");
+
+        // form of type input for text area
+        const inputEl = $("<input>");
+        inputEl.addClass("textarea col-10");
+
+        // button for save button
+        const buttonEl = $("<button>");
+        buttonEl.addClass("saveBtn col-1");
+
+        // append three elements to row
+        rowEl.append(hourEl, inputEl, buttonEl);
+
+        // append row to container
+        $(".container").append(rowEl);
+    }
+}
+
 setCurrentDay();
+createWorkDaySchedule();
